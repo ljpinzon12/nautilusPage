1a98da614340241a57ad0420d47c667adc72b722
Lady Jeraldynne Pinzon Capera <lj.pinzon12@uniandes.edu.co>
2018-11-18 22:13:35 -0500

    Updates

---
+++ precache-manifest.101f176a185009d6fea627afc2b25059.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0ea308b1e381c490ccb",
+    "url": "/nautilusPage/static/js/runtime~main.c0ea308b.js"
+  },
+  {
+    "revision": "1497e42fcdeabf4f4716",
+    "url": "/nautilusPage/static/js/main.1497e42f.chunk.js"
+  },
+  {
+    "revision": "818ae12ee158dc666e25",
+    "url": "/nautilusPage/static/js/1.818ae12e.chunk.js"
+  },
+  {
+    "revision": "1497e42fcdeabf4f4716",
+    "url": "/nautilusPage/static/css/main.9c8b2747.chunk.css"
+  },
+  {
+    "revision": "c2f628d15de67fb8893684bfe9f550ce",
+    "url": "/nautilusPage/index.html"
+  }
+];
